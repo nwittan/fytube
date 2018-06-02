64071a4cf261520411288297e7222b1883a65cd0
unknown <genius24894@gmail.com>
2018-06-02 21:26:47 +0700

    Refix

--- server.js
+++ server.js
@@ -13,8 +13,35 @@ app.get('/video/*', function(req, res){
 	Youtube.getInfo({url: id})
   .then(video => { try {request(video['formats'][0]['url']).pipe(res);}catch(e){res.send("Please recheck  video id ! If it wasn't wrong, perhaps cause by video restriction !");}});
 })
+
+app.get('/', function(req, res){
+    // The URL we will scrape from - in our example Anchorman 2.
+
+    url = 'http://www.imdb.com/title/tt1229340/';
+
+    // The structure of our request call
+    // The first parameter is our URL
+    // The callback function takes 3 parameters, an error, response status code and the html
+
+    request(url, function(error, response, html){
+
+        // First we'll check to make sure no errors occurred when making the request
+
+        if(!error){
+            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+            var $ = cheerio.load(html);
+
+            // Finally, we'll define the variables we're going to capture
+
+            var title, release, rating;
+            var json = { title : "", release : "", rating : ""};
+        }
+    })
+})
+
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
+app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
