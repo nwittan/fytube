77c0e67dc9260daee7ceb7bc09f86a9e14ce290f
unknown <genius24894@gmail.com>
2018-06-02 21:13:05 +0700

    Refix

--- server.js
+++ server.js
@@ -40,7 +40,8 @@ app.get('/', function(req, res){
     })
 })
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log('Express server listening on port', port)
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
